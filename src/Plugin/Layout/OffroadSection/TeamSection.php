4354b7d949652623a7af5f88b3ce162479903134
nzouda777 <rodriguenzouda@gmail.com>
2022-03-31 14:49:37 +0100

    update

--- src/Plugin/Layout/OffroadSection/TeamSection.php
+++ src/Plugin/Layout/OffroadSection/TeamSection.php
@@ -20,6 +20,21 @@ use Drupal\formatage_models\Plugin\Layout\Sections\FormatageModelsSection;
  *  regions = {
  *      "slider_section" = {
  *          "label" = @Translation("add a slider")
+ *      },
+ *      "description_section" = { 
+ *          "label" = @Translation("add a description")
+ *      },
+ *      "single_user_img" = {
+ *          "label" = @Translation("add user image")
+ *      },
+ *      "single_user_info" = {
+ *          "label" = @Translation("set user info")
+ *      },
+ *      "company_name" = {
+ *          "label" = @Translation("set the company name")
+ *      },
+ *      "company_description" = {
+ *          "label" = @Translation("set company description")
  *      }
  * }
  *  
