c952c9859c1745046a2502d226011e528d4e3d5e
nzouda777 <rodriguenzouda@gmail.com>
2022-04-04 13:01:02 +0100

    MAJ

--- src/Plugin/Layout/OffroadSection/RodModTeamSection.php
+++ src/Plugin/Layout/OffroadSection/RodModTeamSection.php
@@ -50,7 +50,7 @@ class RodModTeamSection extends FormatageModelsSection {
   public function __construct(array $configuration, $plugin_id, $plugin_definition, StylesGroupManager $styles_group_manager) {
     // TODO Auto-generated method stub
     parent::__construct($configuration, $plugin_id, $plugin_definition, $styles_group_manager);
-    $this->pluginDefinition->set('icon', drupal_get_path('module', 'formatage_models') . "/icones/sections/");
+    $this->pluginDefinition->set('icon', drupal_get_path('module', 'formatage_models') . "/icones/sections/rod-mod-team-section.png");
   }
   
 }
