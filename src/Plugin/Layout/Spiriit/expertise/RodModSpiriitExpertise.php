d876ec211408620935ed5f786f53a93b1a6259ad
nzouda777 <rodriguenzouda@gmail.com>
2022-07-06 13:29:45 +0100

    full drupal layout intergration

--- src/Plugin/Layout/Spiriit/expertise/RodModSpiriitExpertise.php
+++ src/Plugin/Layout/Spiriit/expertise/RodModSpiriitExpertise.php
@@ -2,6 +2,8 @@
 
 namespace Drupal\rod_mod\Plugin\Layout\Spiriit\expertise;
 
+
+use Drupal\formatage_models\FormatageModelsThemes;
 use Drupal\formatage_models\Plugin\Layout\Sections\FormatageModelsSection;
 use Drupal\bootstrap_styles\StylesGroup\StylesGroupManager;
 
@@ -18,7 +20,7 @@ use Drupal\bootstrap_styles\StylesGroup\StylesGroupManager;
  *  default_region = "small_desc",
  *  regions = {
  *      "section_title" = {
- *          "label" = @Translation("set the section title")
+ *          "label" = @Translation("set the section name")
  *      },
  *      "title" = {
  *          "label" = @Translation("set title")
@@ -42,6 +44,60 @@ use Drupal\bootstrap_styles\StylesGroup\StylesGroupManager;
 
      public function __construct(array $configuration, $plugin_id, $plugin_definition, StylesGroupManager $style_group_manager){
         parent:: __construct($configuration, $plugin_id, $plugin_definition, $style_group_manager);
+        $this->pluginDefinition->set('icon', drupal_get_path('module', 'rod_mod') . "/icones/sections/spiriitexpertise.jpeg");
         
      }
+     /**
+      * {@inheritdoc}
+      * @see \Drupal\formatage_models\Plugin\Layout\FormatageModels::build()
+      */
+
+      public function build(array $regions){
+         //to make auto-generated mehod stub
+          $build = parent::build($regions);
+          FormatageModelsThemes::formatSettingValues($build);
+
+          return $build;
+      }
+      /**
+       * {@inheritdoc}
+       */
+      public function defaultConfiguration(){
+         return parent::defaultConfiguration() + [
+            'css' => '',
+            'Content' => [
+               'builder-form' => true,
+               'info' => [
+                  'title' => 'expertise form section editor',
+                  'loader' => 'static'
+               ],
+               'fields' => [
+                  "section_title" => [
+                     'text_html' => [
+                        'label' => 'nom de la section',
+                        'value' => ''
+                     ]
+                  ],
+                  "title" => [
+                     'text_html' => [
+                        'label' => 'titre de la section',
+                        'value' => ''
+                     ]
+                  ],
+                  "small_desc" => [
+                     'text_html' => [
+                        'label' => 'ecrire une description pour la section',
+                        'value' => ''
+                     ]
+                  ],
+                  "lutin_img" => [
+                     'img_bg' => [
+                        'label' => 'ajouter une image ',
+                        'fids' => []
+                     ]
+                  ]
+               ]
+            ]
+         ];
+      }
  }
